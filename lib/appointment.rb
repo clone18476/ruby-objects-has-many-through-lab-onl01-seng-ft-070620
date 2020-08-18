f28b772588577ebd9ca3081b43ca516d3a91d337
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-18 03:00:15 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,9 @@
 class Appointment
-  attr_accessor :name, :doctor, :patient    # initialize with a name, doctor, and patient,
+  attr_accessor :date, :patient, :doctor    # initialize with a name, doctor, and patient,
                                           # of which none are changeable
   @@all = []     # knows about all appointment  instances 
   
-  def initialize(name, doctor, patient)  # initialize with a name, doctor, and patient
+  def initialize(date, patient, doctor)  # initialize with a name, doctor, and patient
     @name = name         # initialize with name 
     @doctor = doctor      # instance of Appointment belongs to a doctor 
     @patient = patient      # instance of Appointment belongs to a patient
